cbce585b4e17bc7f81af8b0473b92fdcf11386b1
aeroserg <85973477+aeroserg@users.noreply.github.com>
2021-11-09 12:32:18 +0300

    Add files via upload

---
+++ new_year/src/js/send.js
@@ -0,0 +1,21 @@
+$("#form_to").submit(function(e) {
+
+    e.preventDefault(); // avoid to execute the actual submit of the form.
+
+    var form = $(form_to);
+    var url = 'https://ancor.ru/feedback/newy.php';
+    
+    $.ajax({
+           type: "POST",
+           url: url,
+           data: "{akey : 'asdLAKSDMlwq9384asor39qalaflanasa', email : 's.annenkov@ancor.ru'}",
+           contentType: "application/json; charset=utf-8",
+            dataType: "json",
+           success: function(data)
+           {
+               alert(data); // show response from the php script.
+           }
+         });
+
+    
+});
